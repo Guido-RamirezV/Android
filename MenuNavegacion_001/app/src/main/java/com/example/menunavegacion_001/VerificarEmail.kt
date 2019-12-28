63bd441ccb1e141757f5fd9ad98feea7032e966a
Guido Ramírez <guido.ramirez.v@hotmail.com>
2019-12-28 18:13:39 -0300

    Creación y configuración de un nuevo layout
    Se creo un nuevo layout para verificar email.

---
+++ MenuNavegacion_001/app/src/main/java/com/example/menunavegacion_001/VerificarEmail.kt
@@ -0,0 +1,10 @@
+package com.example.menunavegacion_001
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VerificarEmail : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+         setContentView(R.layout.verificar_email)
+    }
+}
